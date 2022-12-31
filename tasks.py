909c1f52df3a434f4487c35c59f161880fb3560a
Omar Carey <omarcarey@omars-mbp-2.lan>
2022-12-30 18:42:26 -0600

    add comments in invoke task

--- tasks.py
+++ tasks.py
@@ -1,4 +1,7 @@
+import tempfile
 from invoke import task
+
+PATH_TO_SPARK_DEPENDENCIES = "/opt/homebrew/Cellar/apache-spark/3.3.1/libexec/"
 
 @task
 def start_local(_):
@@ -7,6 +10,13 @@ def start_local(_):
 
 @task
 def build_docker(ctx):
+    # copy dockerfile into temp_dir
+    # copy spark dependencies into temp_dir
+    # build docker with those spark dependencies
+    # use ctx.cd thing into temp_dir
 
-    ctx.run("docker build -f docker/spark.Dockerfile . -t spark:latest")
-    
+    with tempfile.TemporaryDirectory() as temp_dir:
+        print('created temporary directory', temp_dir)
+
+    # ctx.run("docker build -f docker/spark.Dockerfile . -t spark:latest")
+
