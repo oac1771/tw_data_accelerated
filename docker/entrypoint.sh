deab82ee5977da5f22cd2b2f739ba22f21987491
Omar Carey <omarcarey@omars-mbp-2.lan>
2022-12-31 15:24:56 -0600

    finish bootstraping local spark cluster

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
-libexec/sbin/start-master.sh
+libexec/sbin/$1 $2
 
 while true; do sleep 1000000000; done
--- tasks.py
+++ tasks.py
@@ -6,11 +6,15 @@ PATH_TO_SPARK_DEPENDENCIES = "/opt/homebrew/Cellar/apache-spark/3.3.1/libexec/"
 
 @task
 def start_local(ctx):
-    ctx.run(f'docker run -d -p 8080:8080 spark:latest entrypoint.sh')
+    ctx.run("docker network create --driver bridge spark-net-bridge", hide=True)
+    stdout = ctx.run(f"docker run --name master -d -p 8080:8080 -p 7077:7077 --network spark-net-bridge spark:latest entrypoint.sh start-master.sh", hide=True).stdout
+    container_id = stdout[0:12]
+
+    ctx.run(f"docker run --name worker -d -p 8081:8081 --network spark-net-bridge spark:latest entrypoint.sh start-worker.sh spark://{container_id}:7077", hide=True)
 
 
 @task
-def build_docker(ctx):
+def docker_build(ctx):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         shutil.copytree(src=PATH_TO_SPARK_DEPENDENCIES, dst=f"{temp_dir}/libexec")
