63b44d2f39f23ed766b56c3b8ee8b9129ceb6ba3
Omar Carey <omarcarey@omars-mbp-2.lan>
2023-01-04 18:03:20 -0600

    add option of specifying number of workers

--- tasks.py
+++ tasks.py
@@ -1,13 +1,18 @@
 import tempfile
 import shutil
+
 from invoke import task
+from jinja2 import Environment, FileSystemLoader
 
 PATH_TO_SPARK_DEPENDENCIES = "/opt/homebrew/Cellar/apache-spark/3.3.1/libexec/"
 
 @task
-def start_local(ctx):
-    with ctx.cd("docker"):
-        ctx.run("docker compose up -d")
+def start_local(ctx, number_of_workers=2):
+    rendered_compose = render_compose(number_of_workers=number_of_workers)
+
+    with open("docker/docker-compose.yml", mode="w+", encoding="utf-8") as f:
+        f.write(rendered_compose)
+    ctx.run("docker compose -f docker/docker-compose.yml up -d")
 
 @task
 def stop_local(ctx):
@@ -27,3 +32,10 @@ def docker_build(ctx):
             ctx.run("docker build -f spark.Dockerfile . -t spark:latest")
             ctx.run("docker build -f proxy.Dockerfile . -t proxy:latest --no-cache")
 
+
+def render_compose(number_of_workers):
+    environment = Environment(loader=FileSystemLoader("docker/"))
+    template = environment.get_template("docker-compose.yml.jinja")
+
+    f = template.render(number_of_workers=number_of_workers)
+    return f
